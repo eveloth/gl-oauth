38dc6eaa6b6631a0a65d0152c9f2c1504345ed79
Eveloth <vadim.stepanov.web@gmail.com>
2023-08-15 14:45:14 +0300

    chore: add CORS and auth installers

---
+++ src/OauthShowcase/Options/CorsOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace OauthShowcase.Options;
+
+public class CorsOptions
+{
+    public const string Cors = "Cors";
+    public string RootDomain { get; set; } = default!;
+    public bool IsLocal { get; set; }
+}
